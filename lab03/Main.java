2d9609777f5128d298a58b8920194ff2a8d1f58a
KauanSarzi <kauansarzi57@gmail.com>
2025-02-26 20:23:48 -0300

    Delete lab03 directory

--- lab03/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GerenciadorNomes gNomes = new GerenciadorNomesMem();  // Corrigindo a instância do objeto
-        Ihm ihm = new Ihm(gNomes); // Corrigindo a declaração e inicialização
-        ihm.dialogar(); // Mantendo a chamada do método
-    }
-}
