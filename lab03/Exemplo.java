c5ca52ca1de8bf36c2705e16d757d3d140c6d98b
KauanSarzi <kauansarzi57@gmail.com>
2025-02-26 20:16:24 -0300

    Add files via upload

---
+++ lab03/Exemplo.java
@@ -0,0 +1,11 @@
+public class Exemplo implements A, B {
+    @Override
+    public String metodoA() {
+        return "oi";
+    }
+    
+    @Override
+    public int metodoB() {
+        return 10;
+    }
+}
